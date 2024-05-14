7cdf5b85bc64b56f5c8939c64ae6aa1cb1f026b8
GabrielCh4v35 <115743862+GabrielCh4v35@users.noreply.github.com>
2024-05-13 22:20:59 -0300

    Criando metricas
    criação de métricas

--- server/api.py
+++ server/api.py
@@ -4,13 +4,12 @@ from database import *
 import bcrypt
 
 class ServerApi:
-    def __init__(self, host='127.0.0.1', port=5000):
+    def __init__(self, host='127.0.0.1', port=8080):
         self.host = host
         self.port = port
         self.app = Flask(__name__)
         CORS(self.app)  # Permitindo solicitações de qualquer origem
         self.connector = MySQLConnector()
-
         self.connector.connect()
 
         @self.app.route('/')
@@ -76,6 +75,66 @@ class ServerApi:
                     return 'Usuário não encontrado.'
             except Exception as err:
                 return f'Erro ao fazer login: {err}'
+        # CRIANDO AS METAS ===================================================================================================================================================
+        @self.app.route('/metrics', methods=['POST'])
+        def create_metrics():
+            # Obtendo dados do formulário de METAS
+
+            id_user = 1
+            metric_name = request.form.get('metric_name')
+            unit_measurement = request.form.get('unit_measurement')
+
+            #inserindo os dados na tabela METRICS
+            try:
+                cursor = self.connector.connection.cursor()
+                query = ("INSERT INTO METRICS (USER_ID, METRIC_NAME, UNIT_MEASUREMENT) "
+                         "VALUES (%s, %s, %s)")
+                data = (id_user, metric_name, unit_measurement)
+                cursor.execute(query, data)
+                self.connector.connection.commit()
+                cursor.close()
+                resposta = {"metric_name": metric_name, "unit_measurement": unit_measurement}
+
+                return jsonify(resposta)
+            
+            except Exception  as err:
+                return f'Erro ao criar uma métrica: {err}'
+        
+        #INSERINDO O VALOR DAS DESPESAS =========================================================================================================================================
+
+        @self.app.route('/add_metric_input', methods=['POST'])
+        def add_metric_input():
+            # Obtendo dados do formulário
+            metric_id = request.form.get('metric_id')
+            user_id = request.form.get('user_id')
+            input_value = request.form.get('input_value')
+
+            # Inserindo os dados na tabela METRICS_INPUT
+            try:
+                cursor = self.connector.connection.cursor()
+                query = ("INSERT INTO METRICS_INPUT (METRIC_ID, USER_ID, INPUT_VALUE) "
+                        "VALUES (%s, %s, %s)")
+                data = (metric_id, user_id, input_value)
+                cursor.execute(query, data)
+                self.connector.connection.commit()
+                cursor.close()
+                
+                # Consultando o nome da métrica com base no ID
+                cursor = self.connector.connection.cursor()
+                query = ("SELECT METRIC_NAME FROM METRICS WHERE ID = %s")
+                cursor.execute(query, (metric_id,))
+                metric_name = cursor.fetchone()[0]
+                cursor.close()
+
+                resposta = {
+                    "metric_name": metric_name,
+                    "input_value": input_value
+                }
+
+                return jsonify(resposta)
+
+            except Exception as err:
+                return f'Erro ao adicionar valor da métrica: {err}'
 
     def load(self):
         self.app.run(host=self.host, port=self.port)
