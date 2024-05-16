e6786b68413a5b6669e300b26e3f15f2cfac95d8
GabrielCh4v35 <115743862+GabrielCh4v35@users.noreply.github.com>
2024-05-15 23:33:40 -0300

    Update api.py
    Token de validação de usuário cadastro/login

--- server/api.py
+++ server/api.py
@@ -2,8 +2,16 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from database import *
 import bcrypt
+import jwt
+from jwt import InvalidSignatureError
+import datetime
+from datetime import timezone
+
+
+secret_key = 'e6b8e25c0e90427bbf52b9adfd007c0979fa59387d2de55d486d32550a815e6c'
 
 class ServerApi:
+
     def __init__(self, host='127.0.0.1', port=8080):
         self.host = host
         self.port = port
@@ -20,33 +28,35 @@ class ServerApi:
 
         @self.app.route('/create_user', methods=['POST'])
         def create_user():
+
             # Obtendo os dados enviados como JSON
             data = request.json
             complete_name = data.get('complete_name')
             email = data.get('email')
             password_hash = data.get('password')
             password_hash = self.encrypt_password(password_hash)
-            access_token = "asdasdasdasdasd"
-            refresh_token = "dadasdasdasd"
+            
+            # Gerando Tokens JWT
+            access_token = jwt.encode({'email': email, 'exp': datetime.datetime.now(tz=timezone.utc) + datetime.timedelta(hours=1)}, secret_key)
 
-            print(complete_name)
 
             try:
                 # Inserindo os dados na tabela USERS
                 cursor = self.connector.connection.cursor()
-                query = ("INSERT INTO USERS (COMPLETE_NAME, EMAIL, PASSWORD_HASH, ACCESS_TOKEN, REFRESH_TOKEN) "
-                         "VALUES (%s, %s, %s, %s, %s)")
-                data = (complete_name, email, password_hash, access_token, refresh_token)
+                query = ("INSERT INTO USERS (COMPLETE_NAME, EMAIL, PASSWORD_HASH, ACCESS_TOKEN) "
+                        "VALUES (%s, %s, %s, %s)")
+                data = (complete_name, email, password_hash, access_token)
                 cursor.execute(query, data)
                 self.connector.connection.commit()
                 cursor.close()
                 
-                return jsonify({'return': 'success'})
+                return 'success'
             except Exception  as err:
                 return f'Erro ao criar usuário: {err}'
 
         @self.app.route('/login', methods=['POST'])
         def login():
+
             # Obtendo os dados enviados como JSON
             data = request.json
             email = data.get('email')
@@ -55,7 +65,7 @@ class ServerApi:
             try:
                 # Obtendo os dados do usuário do banco de dados
                 cursor = self.connector.connection.cursor()
-                query = ("SELECT EMAIL, PASSWORD_HASH FROM USERS WHERE EMAIL = %s")
+                query = ("SELECT EMAIL, PASSWORD_HASH, ACCESS_TOKEN FROM USERS WHERE EMAIL = %s")
                 cursor.execute(query, (email,))
                 user = cursor.fetchone()
 
@@ -65,8 +75,15 @@ class ServerApi:
                     
                     # Verificando se a senha fornecida corresponde à senha criptografada armazenada
                     if bcrypt.checkpw(password.encode('utf-8'), senha_criptografada_armazenada.encode('utf-8')):
-                        cursor.close()
-                        return jsonify({'token': 'token_fixo_qualquer'})
+                        access_token = user[2]
+
+                        # Verificando a assinatura do token
+                        try:
+                            cursor.close()
+                            return jsonify({'access_token': access_token.encode('utf-8')})
+                        except InvalidSignatureError:
+                            cursor.close()
+                            return 'Assinatura inválida do token.'                    
                     else:
                         cursor.close()
                         return 'Credenciais inválidas.'
@@ -75,11 +92,14 @@ class ServerApi:
                     return 'Usuário não encontrado.'
             except Exception as err:
                 return f'Erro ao fazer login: {err}'
+            
+            
         # CRIANDO AS METAS ===================================================================================================================================================
         @self.app.route('/metrics', methods=['POST'])
         def create_metrics():
             # Obtendo dados do formulário de METAS
 
+            data = request.json
             id_user = 1
             metric_name = request.form.get('metric_name')
             unit_measurement = request.form.get('unit_measurement')
